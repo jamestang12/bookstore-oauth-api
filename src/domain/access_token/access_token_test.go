bb62c8841a9d721ea716108c6601072bfb9ae72b
jamestang12 <homing.tang@ryerson.ca>
2021-01-09 15:48:40 -0500

    Init Commit & Test Case Created For Token

---
+++ src/domain/access_token/access_token_test.go
@@ -0,0 +1,28 @@
+package access_token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccessTokenConstants(t *testing.T) {
+	assert.EqualValues(t, 24, expirationTime, "expiration time should be 24 hours")
+}
+
+func TestGetNewAccessToken(t *testing.T) {
+	at := GetNewAccessToken()
+	assert.False(t, at.IsExpired(), "brand new axcess token should not be expired")
+	assert.EqualValues(t, "", at.AccessToken, "new axcess token should not be expired")
+	assert.True(t, at.UserId == 0, "new access token should not have an accociated user id")
+}
+
+func TestAccessTokenIsExpired(t *testing.T) {
+	at := AccessToken{}
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
+
+	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	assert.False(t, at.IsExpired(), "access token expiring three hours from now and should NOT be ecpired")
+
+}
